58e5fce3f41058b49385a068d5c35a1f52bacc5d
Luis Silveira <104040064+LuisSilveiraALX@users.noreply.github.com>
2023-09-15 09:34:16 -0300

    retoques finales

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -11,6 +11,8 @@ const Card = ({
                     }) => {
 
                 const [productTitle, setProductTitle] = useState(title);
+                const [fontSize, setFontSize] = useState(15);
+                
                 const { cartItems, addToCart, handleAmountChange } =
                 useContext(ShopContext);
                 const cartItemAmount = cartItems[id]
@@ -18,6 +20,11 @@ const Card = ({
                 const handleTitleChange = (e) => {
                     setProductTitle(e.target.value);
                 };
+
+                const handleFontSizeChange = (e) => {
+                    const newSize = e.target.value;
+                    setFontSize(newSize);
+                  };
 
                 return (
                 <div className="product__container">
@@ -28,13 +35,22 @@ const Card = ({
                     alt={productTitle}
                     />
                 </div>
-                <h1>{productTitle}</h1> 
+                <h1 style={{ fontSize: `${fontSize}px` }}>{productTitle}</h1> 
+                <input
+                    type="range"
+                    min="12"
+                    max="20"
+                    step="1"
+                    value={fontSize}
+                    onChange={handleFontSizeChange}
+                />
                 <input
                     type="text"
                     value={productTitle}
                     onChange={handleTitleChange} 
                     className='input__name'
                 />
+
                 <div className="amount__container">
                     <h2>${price}</h2>
                     <input className='input__number' type="number" value={cartItemAmount} readOnly/>
